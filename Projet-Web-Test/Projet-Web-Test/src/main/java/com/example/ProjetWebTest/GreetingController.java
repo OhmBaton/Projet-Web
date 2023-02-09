54eea4938857ffbde1107271e68901057dc2c0af
OhmBaton <55558758+OhmBaton@users.noreply.github.com>
2023-02-09 16:37:47 +0100

    Add files via upload
    Uploadv2

--- Projet-Web-Test/Projet-Web-Test/src/main/java/com/example/ProjetWebTest/GreetingController.java
+++ Projet-Web-Test/Projet-Web-Test/src/main/java/com/example/ProjetWebTest/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
